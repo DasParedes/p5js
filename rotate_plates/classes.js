eb7c6982d8000ea7710ab682c2aa63e0e2040530
Andrei <andreifilipe.t@hotmail.com>
2018-01-02 03:15:01 -0200

    Bodys now are affects by the plates inside them!

--- rotate_plates/classes.js
+++ rotate_plates/classes.js
@@ -4,6 +4,7 @@ class Plate{
 		this.rotate = vel_rotate;
 		this.angle = 0;
 		this.diameter = diameter;
+		this.shoot = false;
 	}
 
 	draw(){
@@ -14,10 +15,187 @@ class Plate{
 		ellipse(0, 0, this.diameter);
 		stroke(255, 0, 0);
 		line(0, -this.diameter/2, 0, this.diameter/2);
+		line(-this.diameter/2, 0, this.diameter/2, 0);
 		pop();
 	}
 
 	update(){
 		this.angle += this.rotate;
+	}
+
+	isInside(obj){
+		var diameter = this.diameter;
+		// Check if the obj is inside the circle
+		// to Rectangles:
+		/*
+		if( obj.pos.x < (this.pos.x + diameter/2) && obj.pos.y < (this.pos.y + diameter/2) && 
+			obj.pos.x > (this.pos.x - diameter/2) && obj.pos.y > (this.pos.y - diameter/2)		){
+			return true;
+		} else {
+			return false;
+		}*/
+		// To circles:
+		if( this.diameter/2 > createVector(obj.pos.x -this.pos.x, obj.pos.y -this.pos.y ).mag()){
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// movimenta o obj através do carrossel do prato
+	effect_rotation(obj){
+		var distance = createVector((obj.pos.x - this.pos.x),(obj.pos.y - this.pos.y));
+		distance.rotate(this.rotate);
+		obj.pos = createVector(this.pos.x, this.pos.y).add(distance);
+		obj.angle+= -this.rotate;
+	}
+}
+
+
+
+class Body{
+	constructor(x, y){
+		this.pos = createVector(x, y);
+		this.vel = createVector(0, 0);
+		this.accel = createVector(0, 0);
+		this.angle = 0;
+		this.size = 20;
+		this.r = 255;
+		this.g = 0;
+		this.b = 0;
+		this.acertado = false;
+	}
+
+	update(){
+		this.vel.add(this.accel);
+		if(CENTER_BODY){
+			this.pos.add(this.vel.rotate(-this.angle));
+		} else {
+			this.pos.add(this.vel.rotate(this.angle));
+		}
+		this.shoot = false;
+	}
+
+	draw(){
+		if(this.acertado){
+			fill(255, 200, 200);
+		} else {
+			fill(255, 0, 0);
+		}
+		if(CENTER_BODY){
+			push();
+			translate(this.pos.x, this.pos.y);
+			rotate(-this.angle);
+			this.desenha_corpo();
+			pop();
+		} else {
+			push()
+			translate(this.pos.x, this.pos.y);
+			rotate(this.angle);
+			this.desenha_corpo();
+			pop();	
+		}
+		this.acertado = false;
+	}
+
+	desenha_corpo(){
+		var size = this.size;
+		// corpo
+		//fill(255, 0, 0);
+		//fill(this.r, this.g, this.b);
+		rect(-size/2, -size/2, size, size);
+
+		// olho
+		fill(240);
+		ellipse(-4, -5, 5);
+		ellipse(+4, -5, 5);
+	}
+
+	controls(array){
+		this.vel = createVector(0, 0);
+		array.forEach( (item, index, array) => {
+			switch(item.toLowerCase()){
+				case 'w':
+					this.vel.y = -1;
+					break;
+				case 's':
+					this.vel.y = +1;
+					break;
+				case 'd':
+					this.vel.x = +1;
+					break;
+				case 'a':
+					this.vel.x = -1;
+					break;
+				case 'e':
+					this.angle -= 0.1;
+					break;
+				case 'q':
+					this.angle += 0.1;
+					break;
+				case ' ':
+					this.shoot = true;
+					break;
+			}
+		});
+		this.vel.mult(2);
+	}
+
+
+	keyup(tecla){
+			switch(tecla){
+				case 'w':
+					this.vel.y = 0;
+					break;
+				case 's':
+					this.vel.y = 0;
+					break;
+				case 'd':
+					this.vel.x = 0;
+					break;
+				case 'a':
+					this.vel.x = 0;
+					break;
+			}
+	}
+
+	collision(obj){
+		var size = this.size;
+		if( obj.pos.x < (this.pos.x + size/2) && obj.pos.y < (this.pos.y + size/2) &&
+			obj.pos.x > (this.pos.x - size/2) && obj.pos.y > (this.pos.y - size/2)){
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
+class Bullet{
+	constructor(x, y, vel){
+		this.pos = createVector(x, y);
+		this.vel = vel;
+		this.angle = this.vel.heading();
+	}
+
+	draw(){
+		push();
+		translate(this.pos.x, this.pos.y);
+		rotate(this.angle - HALF_PI);
+		fill(255, 0, 0);
+		rect(-1, 0, +1, 10);
+		pop();
+	}
+
+	update(){
+		this.pos.add(this.vel);
+	}
+
+	out_limits(){
+		if( this.pos.x > width 	|| this.pos.x < 0 && 
+			this.pos.y > height 	|| this.pos.y < 0	){
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
--- rotate_plates/main.js
+++ rotate_plates/main.js
@@ -1,20 +1,92 @@
-var plates = [];
+/*
+ * To Do:
+ * Modo 2 jogadores ? 
+ *
+ *
+ *
+ */
 
+
+
+var plates = [];
+var body;
+var bullets = [];
+
+var targets = [];
 
 function setup(){
-	createCanvas(400, 300);
+	createCanvas(600, 600);
 
-	plates.push( new Plate(100, 100, 100, .05));
+		
+	plates.push( new Plate(450, 80, 250, .03));
+	plates.push( new Plate(100, 100, 500, .05));
+	plates.push( new Plate(120, 520, 400, .04));
+	plates.push( new Plate(420, 350, 350, .03));
+	body = new Body(width/2, height/2);
 
-	frameRate(2);
+	targets.push(new Body(width/2 - 50, height/2 - 50));
+	frameRate(30);
 }
 
 function draw(){
 	background(55);
+	push();
+	if(CENTER_BODY){
+		translate(width/2, height/2);
+		rotate(body.angle);
+		translate(-body.pos.x, -body.pos.y);
+	}
+
+	for(var i=0; i<width; i += width/10){
+		line(i, 0, i, height);
+	}
+	for(var j=0; j<height; j+= height/10){
+		line(0, j, width, j);
+	}
 	plates.forEach( (item, index, array ) => {
-		item.draw();
 		item.update();
+		item.draw();
+		if(item.isInside(body)){
+			item.effect_rotation(body);
+		}
+		for(i=0; i<targets.length; i++){
+			if(item.isInside(targets[i])){
+				item.effect_rotation(targets[i]);
+			}
+		}
 	});
 
-	console.log(keys);
+	body.update();
+	body.controls(keys);
+	body.draw();
+	if(body.shoot){
+		bullets.push( new Bullet(body.pos.x, body.pos.y, createVector(0, -10).rotate(-this.body.angle)));
+		console.log("shoot!");
+	}
+
+	bullets.forEach( (b, index, array ) => {
+		b.update();
+		b.draw();
+	});
+
+	targets.forEach( (t) => {
+		t.update();
+		t.draw();
+		bullets.forEach( (b) => {
+			if(t.collision(b)){
+				t.acertado = true;
+				fill(255, 0, 0);
+				text("Bang!", 10, 10, 30);
+			}
+		});
+	});
+
+	
+
+	for(var i=bullets.length; i>0; i--){
+		if(bullets[i-1].out_limits()){
+			//bullets.splice(i, 1);
+		}
+	}
+	pop();
 }
